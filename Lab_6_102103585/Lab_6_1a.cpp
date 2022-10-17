8928ee266012685a3a87fa92a32271b139362b96
rishabhgl <rgoyal2_be21@thapar.edu>
2022-10-17 11:43:02 +0530

    Added Selection Sort Implementation

--- Lab_6_102103585/Lab_6_1a.cpp
+++ Lab_6_102103585/Lab_6_1a.cpp
@@ -6,6 +6,34 @@ using namespace std;
 
 int main()
 {
-    
+    int input,min,temp;
+    cout << "Enter the number of inputs: " << endl;
+    cin >> input;
+    int arr[input];
+    for (int i = 0; i < input; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < input-1; i++)
+    {
+        min = i;
+        for (int j = i+1; j < input; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        if ( i != min )
+        {
+            temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+    }
+    for (int i = 0; i < input; i++)
+    {
+        cout << arr[i] << " ";
+    }
     return 0;
 }

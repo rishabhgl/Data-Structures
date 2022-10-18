2cf4e8fc87666b44cf8ed17e0d4305f5a81d8b03
rishabhgl <rgoyal2_be21@thapar.edu>
2022-10-18 12:14:16 +0530

    Added Counting Sort Implementation

--- Lab_6_102103585/Lab_6_1f.cpp
+++ Lab_6_102103585/Lab_6_1f.cpp
@@ -6,17 +6,40 @@ using namespace std;
 
 int main()
 {
-    int input,temp;
+    int input,k,num = 1, i = 0; 
     cout << "Enter the number of inputs: " << endl;
     cin >> input;
-    int arr[input];
+    int inArr[input];
+    cout << "Enter the maximum number k that is present in the array: " << endl;
+    cin >> k;
+    int freqArr[k+1];
+    int outArr[input];
     for (int i = 0; i < input; i++)
     {
-        cin >> arr[i];
+        cin >> inArr[i];
+    }
+    for (int i  = 0; i <= k ; i++)
+    {
+        freqArr[i] = 0;
     }
     for (int i = 0; i < input; i++)
     {
-        cout << arr[i] << " "; 
+        freqArr[inArr[i]]++;
+    }
+    while (i < input)
+    {
+        if (freqArr[num])
+        {
+            outArr[i] = num;
+            freqArr[num]--;
+            if (!freqArr[num])
+                num++;
+            i++;
+        }
+    }
+    for (int i = 0; i < input; i++)
+    {
+        cout << outArr[i] << " "; 
     }
     return 0;
 }
